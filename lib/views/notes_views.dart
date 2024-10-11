17f1f3ef337e428b3fa18e7e35d2a26178c8c436
habibahossam <habebahossam1195@gmail.com>
2024-10-11 21:00:58 +0300

    add border redius to buttom sheet

--- lib/views/notes_views.dart
+++ lib/views/notes_views.dart
@@ -11,6 +11,8 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
               context: context,
               builder: (context) {
                 return const AddNote();
--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -6,63 +6,66 @@ class AddNote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Title',
-              labelStyle:
-                  TextStyle(color: const Color.fromARGB(255, 182, 245, 225)),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.white),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.white),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Content',
-              labelStyle:
-                  TextStyle(color: const Color.fromARGB(255, 182, 245, 225)),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: const BorderSide(color: Colors.white),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: const BorderSide(color: Colors.white),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Color.fromARGB(255, 182, 245, 225),
-            ),
-            child: MaterialButton(
-              onPressed: () {},
-              child: Text(
-                'Add',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Title',
+                labelStyle:
+                    TextStyle(color: const Color.fromARGB(255, 182, 245, 225)),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.white),
                 ),
               ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 15,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Content',
+                labelStyle:
+                    TextStyle(color: const Color.fromARGB(255, 182, 245, 225)),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color.fromARGB(255, 182, 245, 225),
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
