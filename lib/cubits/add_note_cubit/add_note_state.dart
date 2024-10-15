d51fcd82c7a557192f71dcbd125f554580c8f8b0
habibahossam <habebahossam1195@gmail.com>
2024-10-15 22:15:21 +0300

    add add note state

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -1,3 +1,13 @@
 abstract class AddNoteState {}
 
 class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailed extends AddNoteState {
+  final String errorMessage;
+
+  AddNoteFailed(this.errorMessage);
+}
