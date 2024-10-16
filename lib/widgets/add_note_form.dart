10e7f4fb39e0cf8d24bea2d916ab956fc47c1ccc
habibahossam <habebahossam1195@gmail.com>
2024-10-16 16:25:34 +0300

    add add note cubit to add note buttom sheet

---
+++ lib/widgets/add_note_form.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_buttom.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          CustomTextFormField(
+            onSaved: (value) {
+              title = value;
+            },
+            title: 'Title',
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          CustomTextFormField(
+            onSaved: (value) {
+              content = value;
+            },
+            title: 'Content',
+            maxLines: 5,
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          CustomBottom(
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+            title: 'Add',
+          ),
+        ],
+      ),
+    );
+  }
+}
